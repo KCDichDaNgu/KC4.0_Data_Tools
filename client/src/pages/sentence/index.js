46a77f8ffb9f9b04e8cf797069a394e74ea0eec4
nqminh <nevermore164@gmail.com>
2020-12-30 18:30:05 +0700

    chore: round score

--- client/src/pages/sentence/index.js
+++ client/src/pages/sentence/index.js
@@ -23,134 +23,14 @@ import { useTranslation } from 'react-i18next';
 const moment = require("moment");
 
 const { TextArea } = Input;
+const { Option } = Select;
 
 const SentencePage = (props) => {
-  const { t, i18n } = useTranslation(['common']);
 
-  const [data, setData] = useState([
-    {
-      key: "1",
-      id: 1,
-      domain: "vov.vn",
-      lang_1: "en-US",
-      lang_2: "vi-VI",
-      text_1_lang:
-        "He was chosen to serve as a political hostage in the Kingdom of Zhao",
-      text_2_lang: "Ông phải làm con tin chính trị ở kinh đô nước Triệu",
-      score: 0.4,
-      last_update: 1608488868000,
-    },
-    {
-      key: "2",
-      id: 2,
-      domain: "vov.vn",
-      lang_1: "en-US",
-      lang_2: "vi-VI",
-      text_1_lang:
-        "People happy for outcomes of Party Central Committee’s 14th session",
-      text_2_lang:
-        "Người dân cả nước phấn khởi trước kết quả Hội nghị Trung ương 14",
-      score: 0.4,
-      last_update: 1608488868000,
-    },
-    {
-      key: "3",
-      id: 3,
-      domain: "vnexpress.net",
-      lang_1: "en-US",
-      lang_2: "zh-CN",
-      text_1_lang: "The programme of support for arts and crafts design",
-      score: 0.9,
-      text_2_lang: "扶持艺术和工艺设计项目",
-      last_update: 1608488868000,
-    },
-    {
-      key: "4",
-      id: 4,
-      domain: "vtv.vn",
-      lang_1: "en-US",
-      lang_2: "zh-CN",
-      text_1_lang: "The Nazis steal the Ark and capture Indiana and Marion",
-      score: 0.8,
-      text_2_lang: "纳粹偷了法柜,并抓住了印第安纳和马里昂",
-      last_update: 1608488868000,
-    },
-    {
-      key: "5",
-      id: 5,
-      domain: "vtv.vn",
-      lang_1: "vi-VI",
-      lang_2: "en-US",
-      text_1_lang: "Có lẽ do hình thái di truyền của hệ thống miễn dịch.",
-      text_2_lang:
-        "It is probably due to the genetic morphology of the immune system.",
-      score: 0.7,
-      last_update: 1608488868000,
-    },
-    {
-      key: "6",
-      id: 6,
-      domain: "vnexpress.net",
-      lang_1: "vi-VI",
-      lang_2: "en-US",
-      text_1_lang:
-        "Các thiết kế không chính thống của Tiến sĩ Porsche ít được ưa chuộng.",
-      text_2_lang: "Dr. Porsche 's unorthodox designs gathered little favour.",
-      score: 0.6,
-      last_update: 1608488868000,
-    },
-    {
-      key: "7",
-      id: 7,
-      domain: "vov.vn",
-      lang_1: "vi-VI",
-      lang_2: "pt-PT",
-      text_1_lang: "Tôi gửi kèm cái bật lửa như một món quà chia tay.",
-      text_2_lang: "Anexei o meu isqueiro como presente de despedida para ti",
-      score: 0.7,
-      last_update: 1608488868000,
-    },
-    {
-      key: "8",
-      id: 8,
-      domain: "vov.vn",
-      lang_1: "zh-CN",
-      lang_2: "vi-VI",
-      text_1_lang:
-        "1929年他毕业于托卢卡的科学和文学研究所,在那里他成为社会主义工党的代表和学生领袖。",
-      text_2_lang:
-        "Năm 1929, ông tốt nghiệp Học viện Khoa học và Văn học Toluca, nơi ông là một đại biểu và là lãnh đạo sinh viên của Đảng Lao động Xã hội Chủ nghĩa.",
-      score: 0.9,
-      last_update: 1608488868000,
-    },
-    {
-      key: "9",
-      id: 9,
-      domain: "vnexpress.net",
-      lang_1: "zh-CN",
-      lang_2: "vi-VI",
-      text_1_lang: "新皇 登基 之前 别 让 宰相 接触 她",
-      text_2_lang:
-        "Giữ con bé tránh xa Thừa Tướng tới khi lễ lên ngôi kết thúc.",
-      score: 0.7,
-      last_update: 1608488868000,
-    },
-    {
-      key: "10",
-      id: 10,
-      domain: "vov.vn",
-      lang_1: "zh-CN",
-      lang_2: "vi-VI",
-      text_1_lang: "不過科學家仍然不清楚這些藍鯨在哪裡過冬。",
-      text_2_lang:
-        "Các nhà khoa học không biết những con cá voi xanh này sống ở đâu vào mùa đông.",
-      score: 0.4,
-      last_update: 1608488868000,
-    },
-  ]);
-  const [dataSource, setDataSource] = useState(data);
+  const { t } = useTranslation(['common']);
+  
+  const [dataSource, setDataSource] = useState([]);
 
-  const [count, setCount] = useState(data.length + 1);
   const [value, setValue] = useState("");
   const [selectedDomain, setSeletedDomain] = useState([]);
   const [isAdding, setIsAdding] = useState(false);
@@ -215,7 +95,7 @@ const SentencePage = (props) => {
       title: t('sentence.score'),
       dataIndex: "score",
       key: "score.senAlign",
-      render: (score) => Number(score['senAlign']).toFixed(4),
+      render: (score) => Number(score['senAlign']).toFixed(2),
       sorter: (a, b, sortOrder) => {},
       sortDirections: ['ascend', 'descend', 'ascend']
     },
@@ -447,21 +327,13 @@ const SentencePage = (props) => {
               </div>
 
               <Button
-                showSearchshowSearch
+                showsearchshowsearch="true"
                 style={{ width: "100px", marginLeft: "30px", background: '#384AD7', borderColor: '#384AD7'}}
                 type="primary"
                 onClick={handleFilter}>
                 { t('sentence.filter') }
               </Button>
             </div>
-
-            {/* <div style={{ float: 'right' }}>
-                            <Button style={{ marginLeft: '12px' }}>Add</Button>
-
-                            <Button style={{ marginLeft: '12px' }}>
-                                Approve
-                            </Button>
-                        </div> */}
           </div>
           <Table
             className="table-striped-rows"
