d17ce859fd62def2a786e5bb3878a9b01b95ca3d
Nguyen Quang Minh <nevermore164@gmail.com>
2021-01-18 13:33:19 +0700

    fix: hot-fix

--- server/app.py
+++ server/app.py
@@ -306,5 +306,5 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=6011,
-        debug=True
+        debug=False
     )
--- server/database/rake/16-01-2021/update_backup_timestamp.py
+++ server/database/rake/16-01-2021/update_backup_timestamp.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     db = client['data-tool']
 
-    for backup in db.backups.find():
+    for backup in db.backup.find():
         hash_name = backup['hash_name']
-        db.backups.update({'_id': backup['_id']}, {'$set': {'created_at': float(hash_name)}})
+        db.backup.update({'_id': backup['_id']}, {'$set': {'created_at': float(hash_name[:-3])}})
 
