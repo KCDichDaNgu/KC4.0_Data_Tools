2d5dd6dccf4612fc29abaa0002ac69d59c9c6b70
tranlinh19021320 <19021320@vnu.edu.vn>
2022-04-20 20:58:30 +0700

    9_APITesting

--- tests/test_api_pair_text/main.py
+++ tests/test_api_pair_text/main.py
@@ -1,33 +1,35 @@
 
-
-from email import header
-from urllib import response
 import requests
 import json
 import unittest2
 import os
 
+def read(URL):
+    response = requests.get(URL, stream= True)
+    if response.status_code != 200:
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as exception:
+            return None, response.status_code, exception
+    return response, response.status_code, "OK"
 class TestAPIPairText(unittest2.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestAPIPairText, self).__init__(*args, **kwargs)
         self.host_url = "http://localhost:3005/document"
-        self.header = {  "X-Powered-By": "Express", 
-            "Accept-Ranges": "bytes",
-            "Content-Type": "application/json; charset=UTF-8",
-            "Vary": "Accept-Encoding",
-            "Content-Encoding": "gzip",
-            "Connection": "keep-alive",
-            "Transfer-Encoding": "chunked"}
         self.response = requests.get(self.host_url)
 
     def test_response_status_code(self):
         self.assertEqual(self.response.status_code, 200, "Faild to connecet url:".format(self.host_url))
-        print("Ok")
     
     def test_header_of_response(self):
         self.assertEqual(self.response.headers.get("Content-Type"), "application/json", 
         "Content Type Error")
     
+    def test_valid_requests(self):
+        url = "http://localhost:3005/doument"
+        response, status_code, message = read(url)
+        self.assertEqual(status_code, 404, "Faild at status code")
+        self.assertEqual(str(message), "404 Client Error: Not Found for url: {}".format(url), "Message error")
 
 if __name__ == "__name__":
     unittest2.main(verbosity=2)
